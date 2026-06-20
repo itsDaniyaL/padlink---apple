6301f5ffe29e13f97d094929daf73156f4fedc50
Daniyal Ahmad Rizwan <daniyal.ahmad.rizwan@hotmail.com>
2026-06-20 19:41:38 +0200

    Initial Commit

---
+++ padlink - ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  padlink - ios
+//
+//  Created by Daniyal Ahmad Rizwan on 20/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
